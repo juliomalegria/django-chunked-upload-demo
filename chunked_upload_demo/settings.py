eeb3409df6ac618bb893b77eba48e0cdfd29875f
Jerin Peter George <jerinpetergeorge@gmail.com>
2019-12-08 20:58:14 +0530

    static files rearranged

--- chunked_upload_demo/settings.py
+++ chunked_upload_demo/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '268=4zc_-mjcasc#(q==71dvndydqo*jlko!eumm&p&4u%w2nv'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'chunked_upload_demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'demo/../templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chunked_upload_demo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -116,8 +111,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static/'
+
+MEDIA_ROOT = 'media/'
--- chunked_upload_demo/urls.py
+++ chunked_upload_demo/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('demo.urls'))
-]
+                  path('', include('demo.urls')),
+                  path('admin/', admin.site.urls),
+
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
